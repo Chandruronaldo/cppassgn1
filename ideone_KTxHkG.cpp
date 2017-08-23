d33d426cdc1650de95f4cad1dd9f151cba474d38
Chandruronaldo <31179796+Chandruronaldo@users.noreply.github.com>
2017-08-23 16:05:39 +0530

    Update ideone_KTxHkG.cpp

--- ideone_KTxHkG.cpp
+++ ideone_KTxHkG.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {	float a,b,p;
 	cout<<"enter the length and breadth of the rectangle";
-	cin>>a,b;
+	cin>>a>>b;
 	p=a*b;
 	cout<<"the area is "<<p;
 	// your code goes here
